5e2c6e764bae727b476876f6db1226f0eebd6ed7
Meet Raval <156414953+MeetRaval8983@users.noreply.github.com>
2026-07-23 16:42:59 +0000

    Coversion and comparison

---
+++ comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 == 1)
+// console.log(2 < 1)
+// console.log(2 != 1)
+
+// console.log("2" < 1)
+// console.log("02" > 1)
+
+// console.log(null < 0)
+// // comparisons convert null to 0, so this is true
+// console.log(null >= 0) 
+// console.log(null == 0)
+
+// console.log(undefined > 0)
+// console.log(undefined <= 0)
+// console.log(undefined == 0)
+
+console.log("2" === 1)
+console.log("1" === 1)
+console.log(2 === 1)
+console.log(1 === 1)
---
+++ conversionOperation.js
@@ -0,0 +1,31 @@
+let value = 3
+console.log(value);
+let negvalue = - value;
+console.log(negvalue);
+
+let s1 = "Hi"
+let s2 = "Hello"
+let s3 = s1 + " " + s2;
+console.log(s3);
+
+console.log(1 + 2)
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + "2")
+console.log(1 + 2 + "3")
+console.log("1" + 2 + 3)
+console.log("1" + (2 + 3))
+
+console.log(1 + 2 * 6 / 3)
+console.log( (3 + 4) * 15 / 3);
+
+console.log(true);
+console.log(+true)
+console.log(+"")
+
+let num1 = num2 = num3 = 2 * 2;
+console.log(num1, num2, num3);
+
+let num = 100
+num++
+console.log(num);
