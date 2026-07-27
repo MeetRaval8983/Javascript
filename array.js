1dea2bbbb72dfea8fd2ac678c9779ef3063d5b64
Meet Raval <156414953+MeetRaval8983@users.noreply.github.com>
2026-07-27 15:04:33 +0000

    Array in detail

--- array.js
+++ array.js
@@ -37,3 +37,39 @@ console.log("B ", myarr);
 const myn2 = myarr.splice(1, 3)
 console.log("C ", myarr);
 console.log(myn2);
+
+const a = ["thor", "ironman", "hulk", "captain america"]
+const b = ["loki", "thanos", "hela"]
+
+// a.push(b)
+// console.log(a)
+// console.log(a[4][1]) // thanos
+
+// a.concat(b)
+// console.log(a) // ["thor", "ironman", "hulk", "captain america"]
+
+// const c = a.concat(b)
+// console.log(c)
+
+// spread operator
+const d = [...a, ...b]
+console.log(d)
+
+const e = [...a, "spiderman", ...b]
+console.log(e)
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+console.log(another_array);
+
+
+const flat_array = another_array.flat(Infinity)
+console.log(flat_array)
+
+console.log(Array.isArray("abc"))
+console.log(Array.from("abc"))
+console.log(Array.from({name: "John"}))
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1, score2, score3))
